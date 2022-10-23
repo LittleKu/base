9d5b68e272dcf6a25ddb1a5bf5f6777929760e86
LittleKu <kklvzl@gmail.com>
2022-10-23 19:03:02 +0800

    add shared and static library gn template

--- examples/execute.cc
+++ examples/execute.cc
@@ -1,11 +1,20 @@
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/path_service.h"
 
-int main(int argc, char **argv) {
-    base::CommandLine::Init(argc, argv);
+int main(int argc, char** argv) {
+  base::CommandLine::Init(argc, argv);
 
-    base::AtExitManager exit_manager;
+  base::AtExitManager exit_manager;
 
-    return EXIT_SUCCESS;
+  logging::LoggingSettings logging_settings;
+  logging_settings.logging_dest = logging::LOG_DEFAULT;
+
+  logging::InitLogging(logging_settings);
+
+  LOG(INFO) << "test info log.";
+  return EXIT_SUCCESS;
 }
