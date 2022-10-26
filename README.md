85b627a37ee6392db74b3b3cf3aba860241932c5
LittleKu <lvzhaolang@cmcm.com>
2022-10-26 12:26:15 +0800

    1.update for windows platform;2.add clang-format;3.update README.md

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# base
-chromium base
+# Chromium base
+## Install depot_tools
+Install depot_tools [here](https://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up)
 
+## Setting up Windows
+
+### Visual Studio
+*   Install [Visual Studio 2019](https://docs.microsoft.com/en-us/visualstudio/releases/2019/release-notes) (>=16.0.0)
+
+*   Install [Windows 10 SDK](https://developer.microsoft.com/en-us/windows/downloads/sdk-archive/)
+
+## Setting up Mac OS
+*   A Mac, Intel or Arm.
+    ([More details about Arm Macs](https://chromium.googlesource.com/chromium/src.git/+/main/docs/mac_arm64.md).)
+*   [Xcode](https://developer.apple.com/xcode/). Xcode comes with...
+*   The macOS SDK. Run
+
+command:
 ```
 gclient config --unmanaged --name=src https://github.com/LittleKu/base.git
 
@@ -8,6 +23,13 @@ gclient sync
 
 cd src
 
-gn gen out\Build --filters://base "--args=is_debug=true" --ide=xcode
+set NINJA_SUMMARIZE_BUILD=1
 
-autoninja -C out\Build default
+# MacOS:
+gn gen out/Debug --filters://base "--args=is_debug=true target_os=\"mac\"" --ide=xcode
+autoninja -C out/Debug default
+
+# Windows:
+gn gen out/Debug_x86 --filters://base "--args=is_debug=true target_os=\"win\" target_cpu=\"x86\"" --ide=vs2019
+autoninja -C out/Debug_x86 default
+```
--- examples/execute.cc
+++ examples/execute.cc
@@ -1,20 +1,49 @@
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
+#include "build/build_config.h"
 
+#if defined(OS_WIN)
+#include <Windows.h>
+#endif
+
+
+#if defined(OS_WIN)
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, LPWSTR command_line, int cmd_show) {
+	base::CommandLine::Init(0, nullptr);// on windows call ParseFromString(::GetCommandLineW());
+#else
 int main(int argc, char** argv) {
-  base::CommandLine::Init(argc, argv);
+	base::CommandLine::Init(argc, argv);// otherwise call InitFromArgv(argc, argv);
+#endif
 
-  base::AtExitManager exit_manager;
+	base::AtExitManager exit_manager;
 
-  logging::LoggingSettings logging_settings;
-  logging_settings.logging_dest = logging::LOG_DEFAULT;
+	logging::LoggingSettings logging_settings;
+	logging_settings.logging_dest = logging::LOG_DEFAULT;
 
-  logging::InitLogging(logging_settings);
+#if defined(OS_WIN) // on windows LOG_DEFAULT is LOG_TO_FILE
+	base::FilePath log_file_path;
+	if (logging_settings.logging_dest & logging::LOG_TO_FILE)
+	{
+		base::PathService::Get(base::FILE_EXE, &log_file_path);
+		log_file_path = log_file_path.RemoveExtension().AddExtension(L".log");
+		logging_settings.log_file_path = log_file_path.value().c_str();
+	}
+#endif
 
-  LOG(INFO) << "test info log.";
-  return EXIT_SUCCESS;
+	{
+		base::FilePath log_prefix;
+		base::PathService::Get(base::FILE_EXE, &log_prefix);
+		logging::SetLogPrefix(log_prefix.RemoveExtension().BaseName().AsUTF8Unsafe().c_str());
+	}
+
+	logging::SetLogItems(true, true, true, true);
+	logging::InitLogging(logging_settings);
+
+	LOG(INFO) << "test info log.";
+	return 0;
 }
